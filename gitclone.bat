b22bc453c82d29d5e07abc3f5a9e697f6e0c6d77
declanmcgee01 <declanmcgee1995@gmail.com>
2019-02-15 15:24:42 +0000

    removed clone file

--- gitclone.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-git clone https://github.com/alvinjo/AccountFront.git
-git clone https://github.com/alvinjo/RealAccountApi.git
-git clone https://github.com/alvinjo/RealAccountNumGenApi.git
-git clone https://github.com/alvinjo/RealAccountConsumer.git
-cd AccountFront
-npm install
-cd ..
